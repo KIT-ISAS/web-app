9c1db4b67c6eb4056a28a9f3a2de36a4539ee4ec
Vlad Korsakov <ulqba@student.kit.edu>
2025-10-15 14:05:33 +0200

    added von mises fisher (via scypy)

--- model/distributions/sphere/vonmises_fisher.py
+++ model/distributions/sphere/vonmises_fisher.py
@@ -2,23 +2,25 @@ from abc import ABC, abstractmethod
 from model.distributions.sphere.sphere_distribution import SphereDistribution
 from util.selectors.slider import Slider 
 import numpy as np
+import scipy
 
 class SphereDistribution(SphereDistribution):
 	def __init__(self):
 		self.sample_options = [
-			Slider("Number of Samples", 1, 5, 15)
+			Slider("Number of Samples", 1, 50, 1000),
+			Slider("Kappa (κ)", 1, 5, 15),
 		]
 		
 		self.distribution_options = []
 
 	def get_name(self):
-		return "Random2"
+		return "von Mises-Fisher"
 	
 	def sample(self, sample_options):
 		sample_count = sample_options[0].state
+		kappa = sample_options[1].state
 
-		samples = np.random.normal(size=(sample_count, 3))
-		samples /= np.linalg.norm(samples, axis=1)[:, np.newaxis]
+		samples = scipy.stats.vonmises_fisher.rvs(mu=[0,0,1], kappa=kappa, size=sample_count)
 
 		return samples
 
