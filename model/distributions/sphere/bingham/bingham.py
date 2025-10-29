301f005aa7550196ee474d55e38d1d6ff4a40d16
Vlad Korsakov <ulqba@student.kit.edu>
2025-10-29 14:04:54 +0100

    added bingham pdf

--- model/distributions/sphere/bingham/bingham.py
+++ model/distributions/sphere/bingham/bingham.py
@@ -3,6 +3,10 @@ from model.distributions.sphere.sphere_distribution import SphereDistribution
 from util.selectors.slider_float import FloatSlider 
 import numpy as np
 
+import pyrecest._backend
+from pyrecest.backend import array
+from pyrecest.distributions import BinghamDistribution
+
 from model.distributions.sphere.bingham.random import BinghamRandomSampling
 
 
@@ -21,4 +25,16 @@ class BinghampDistribution(SphereDistribution):
 		return "Bingham"
 
 	def get_pdf(self, distribution_options):			
-		return None
+		l1 = distribution_options[0].state
+		l2 = distribution_options[1].state
+
+		lambdas = np.sort(np.array([l1, l2, 0]))[::-1]
+		lambdas = -lambdas
+		print(lambdas)
+		M = np.eye(3)
+		bingham_dist = BinghamDistribution(M=array(M), Z=array(lambdas))
+
+
+		def pdf(x):
+			return bingham_dist.pdf(array(x))
+		return pdf
