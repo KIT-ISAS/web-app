c97b3cbceefe6a0647761e46d885f521719f1259
Vlad Korsakov <ulqba@student.kit.edu>
2025-11-20 17:40:45 +0100

    add kronecker to torus with warning

---
+++ model/distributions/torus/uniform/fibonacci_kronecker.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+from util.selectors.silder_log import LogSlider 
+from model.distributions.torus.torus_sampling_schema import TorusSamplingSchema
+
+class TorusKroneckerUniformSampling(TorusSamplingSchema):
+	def __init__(self):
+		self.sample_options = [
+			LogSlider("Number of Samples", 10, 100, 10000)
+		]
+		self.info_md = """
+		> Warning: Mapping the Kronecker lattice to the torus is not recommended in practice, as it is only periodic on one axis. 
+		It is included for educational purposes only."""
+
+	def get_name(self):
+		return "Fibonacci-Kronecker Lattice"
+	
+	def sample(self, sample_options, distribution_options):
+		sample_count = sample_options[0].state
+
+		indices = np.arange(0, sample_count)
+		gol = (1+5**0.5)/2
+		
+		# centered rank-1 lattice generator
+		equidistant_generator = (2 * indices + 1) / (2 * sample_count)
+		
+		z = equidistant_generator
+		p = (indices / gol) % 1
+
+		return np.column_stack((p * 2 * np.pi, z * 2 * np.pi))
--- model/distributions/torus/uniform/uniform.py
+++ model/distributions/torus/uniform/uniform.py
@@ -5,14 +5,16 @@ import numpy as np
 from model.distributions.torus.torus_distribution import TorusDistribution
 from model.distributions.torus.uniform.random import TorusRandomUniformSampling
 from model.distributions.torus.uniform.fibonacci_rank_1 import TorusFibRank1UniformSampling
-
+from model.distributions.torus.uniform.fibonacci_kronecker import TorusKroneckerUniformSampling
 class UniformTorusDistribution(TorusDistribution):
 	def __init__(self):
 		self.distribution_options = []
 		self.sampling_methods = [
 			TorusRandomUniformSampling(),
 			TorusFibRank1UniformSampling(),
+			TorusKroneckerUniformSampling(),
 		]
+		
 
 	def get_name(self):
 		return "Uniform"
